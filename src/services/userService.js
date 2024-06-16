648832026b93cf224950e6d465abaaf4ddc5abd1
SmoKINGcrimSON <ferchowarrior169@gmail.com>
2024-06-16 16:09:24 -0500

    fourth commit

--- src/services/userService.js
+++ src/services/userService.js
@@ -25,6 +25,7 @@ export class UserService{
             res.status(200).json(user)
     }
     async create(req, res){
+        /*
         const body = req.body
         body.register_date = new Date(body.register_date)
         const result = validateUser(body)
@@ -37,6 +38,25 @@ export class UserService{
         return user === null? 
             res.status(400).json({"Error": "resource not created"}) : 
             res.json({user: user, token: token})
+            */
+            try {
+                const body = req.body;
+                body.register_date = new Date(body.register_date);
+                const result = validateUser(body);
+                if (result.error) {
+                    console.error('Error de validación:', result.error);
+                    return res.status(400).json({ mess: JSON.parse(result.error.message) });
+                }
+                const user = await this.repository.create({ body: body });
+                const token = jwt.sign({ id: user.id }, secret, { expiresIn: '1h' });
+                console.log(token);
+                return user === null ? 
+                    res.status(400).json({ "Error": "Recurso no creado" }) : 
+                    res.json({ user: user, token: token });
+            } catch (error) {
+                console.error('Error creando usuario:', error);
+                return res.status(500).json({ message: 'Error interno del servidor' });
+            }
     }
     async deleteById(req, res){
         const id = req.id
